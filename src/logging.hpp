452a95dfff32306f3cc207823ba47595caab9995
oosiriiss <oosiriiss@gmail.com>
2025-08-24 00:55:20 +0200

    Improved logging by introducing varargs handling directly in the log maco

--- src/logging.hpp
+++ src/logging.hpp
@@ -1,16 +1,22 @@
 #pragma once
 
+#include <format>
 #ifdef CPPLI_ENABLE_LOG
 #include <print>
 #include <string_view>
 
-#define CPPLI_LOG_DEBUG(msg) logInternal(__FILE__, __LINE__, msg)
+#define CPPLI_LOG_DEBUG(fmt, ...)                                              \
+  logInternalFormat(__FILE__, __LINE__, fmt, ##__VA_ARGS__)
 
-void logInternal(std::string_view file, size_t line, std::string msg) {
-  std::println("{}:{} | {}", file, line, msg);
+template <typename... Args>
+inline void logInternalFormat(const std::string_view file, size_t line,
+                              const std::string_view fmt, Args &&...args) {
+  // prepend file:line, then append fmt
+  std::println("{}:{} | {}", file, line,
+               std::vformat(fmt, std::make_format_args(args...)));
 }
 
 #else
-#define CPPLI_LOG_DEBUG(msg)
+#define CPPLI_LOG_DEBUG(fmt, ...)
 
 #endif
