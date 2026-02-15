59e94b018a57b27012b6fd0a0e79a7d71c30ec74
oosiriiss <oosiriiss@gmail.com>
2026-02-15 20:14:09 +0100

    ArgumentContainer bidirectional iterator

--- src/argument_container.hpp
+++ src/argument_container.hpp
@@ -1,6 +1,6 @@
 #pragma once
 #include <cassert>
-#include <stdexcept>
+#include <iterator>
 #include <string_view>
 
 #include "debug_utils.hpp"
@@ -28,4 +28,50 @@ struct ArgumentContainer {
  private:
   int argc_{0};
   char const* const* argv_{nullptr};
-};
+
+ public:
+  struct ArgIterator {
+    using iterator_category = std::bidirectional_iterator_tag;  // NOLINT
+    using difference_type = std::ptrdiff_t;                     // NOLINT
+    using value_type = std::string_view;                        // NOLINT
+
+    ArgIterator() = default;
+    ArgIterator(const char* const* data)  // NOLINT
+        : data_(data) {}
+
+    [[nodiscard]] constexpr auto operator*() const -> value_type {
+      return std::string_view(*data_);
+    }
+    [[nodiscard]] constexpr auto operator++() -> ArgIterator& {
+      ++data_;
+      return *this;
+    }
+    [[nodiscard]] constexpr auto operator++(int) -> ArgIterator {
+      ArgIterator temp = *this;
+      ++data_;
+      return temp;
+    }
+    [[nodiscard]] constexpr auto operator--() -> ArgIterator& {
+      --data_;
+      return *this;
+    }
+    [[nodiscard]] constexpr auto operator--(int) -> ArgIterator {
+      ArgIterator temp = *this;
+      --data_;
+      return temp;
+    }
+
+    constexpr auto operator==(const ArgIterator& other) const -> bool {
+      return data_ == other.data_;
+    }
+
+   private:
+    const char* const* data_{nullptr};
+  };
+
+  using iterator = ArgIterator;  // NOLINT
+
+  constexpr auto begin() -> iterator { return iterator(argv_); }
+  constexpr auto end() -> iterator { return iterator(argv_ + argc_); }
+
+};  // NOLINT
--- test/arg_container_test.cpp
+++ test/arg_container_test.cpp
@@ -1,13 +1,19 @@
 
 #include <array>
-#include <string>
+#include <cmath>
+#include <cstdlib>
+#include <print>
+#include <queue>
+#include <ranges>
+#include <stack>
 #include <tasty/errors.hpp>
 #include <tasty/runners.hpp>
 #include <tasty/tasty.hpp>
 
 #include "argument_container.hpp"
+#include "debug_utils.hpp"
 
-auto main() -> int {
+static constexpr void properAccesToArguments() {
   std::array argv1 = {
       "./program",
       "arg",
@@ -16,17 +22,71 @@ auto main() -> int {
   };
 
   ArgumentContainer args1(argv1.size(), argv1.data());
+  tasty::expectEqual(args1.count(), argv1.size());
+  tasty::expectEqual(args1[0], argv1[0]);
+  tasty::expectEqual(args1[1], argv1[1]);
+  tasty::expectEqual(args1[2], argv1[2]);
+  tasty::expectEqual(args1[3], argv1[3]);
 
-  try {
-    tasty::expectEqual(args1.count(), argv1.size());
-    tasty::expectEqual(args1[0], argv1[0]);
-    tasty::expectEqual(args1[1], argv1[1]);
-    tasty::expectEqual(args1[2], argv1[2]);
-    tasty::expectEqual(args1[3], argv1[3]);
-    tasty::expectEqual(args1[4], argv1[3]);
-  } catch (const tasty::errors::ExpectFailed& err) {
-    return -1;
+  // This should throw, and for now operator[] only throws in debug build only
+  // as this error is purely skill issue related, and related only to the
+  // developer and not the users
+  // tasty::expectEqual(args1[4], argv1[3]);
+}
+
+static constexpr void properForwardIterationOfArguments() {
+  std::array argv = {
+      "./program",
+      "arg",
+      "--flag",
+      "--option=value",
+  };
+  std::queue<std::string_view> queue;
+  queue.push_range(argv);
+
+  ArgumentContainer args(argv.size(), argv.data());
+
+  for (std::string_view actual : args) {
+    std::string_view expected = queue.front();
+    queue.pop();
+    std::print("{}", expected);
+    tasty::expectEqual(expected, actual);
+  }
+  std::println();
+}
+static constexpr void properBackwardIterationOfArguments() {
+  std::array argv = {
+      "./program",
+      "arg",
+      "--flag",
+      "--option=value",
+  };
+  std::stack<std::string_view> stack;
+  stack.push_range(argv);
+
+  ArgumentContainer args(argv.size(), argv.data());
+
+  for (std::string_view actual : args | std::views::reverse) {
+    std::string_view expected = stack.top();
+    stack.pop();
+    std::print("{}", expected);
+    tasty::expectEqual(expected, actual);
+  }
+  std::println();
+}
+
+auto main() -> int {
+  tasty::TestRunner runner("ArgumentContainer tests");
+
+  runner.registerTest(properAccesToArguments, "properAccesToArguments");
+  runner.registerTest(properForwardIterationOfArguments,
+                      "properForwardIterationOfArguments");
+  runner.registerTest(properBackwardIterationOfArguments,
+                      "properBackwardIterationOfArguments");
+
+  if (!runner.runAll()) {
+    return EXIT_FAILURE;
   }
 
-  return 0;
+  return EXIT_SUCCESS;
 }
