3ae75b187f60b5d8aa9484aa9f90176149a9b951
oosiriiss <oosiriiss@gmail.com>
2026-04-02 21:13:43 +0200

    Document examples

---
+++ example/general.cpp
@@ -0,0 +1,56 @@
+#include <cstdlib>
+#include <iostream>
+#include <print>
+
+#include "cppli/cppli.hpp"
+
+enum class ArgumentType : std::uint8_t { Help, Output };
+
+auto main(int argc, char** argv) -> int {
+  // We create option container with any type out want (that support
+  // comparisons)
+  // In my opion enum classes are perfect fit for this use case.
+  cppli::OptionContainer<ArgumentType> options;
+
+  // Define your options
+  // firstName and secondName are the names that will be used to "recognize" the
+  // option.
+  // firstName is considered to be the "short" name and second name to be the
+  // "lon " name, but it is not enforced.
+  // description is used for automatic generation of help message.
+  // needsValue determines whether an option will need a value for example
+  // --out main or --out=main
+  options.addOption(ArgumentType::Help,
+                    cppli::Option{
+                        .firstName = "-h",
+                        .secondName = "--help",
+                        .description = "Displays help message",
+                        .needsValue = false,
+                    });
+  options.addOption(ArgumentType::Output,
+                    cppli::Option{.firstName = "",
+                                  .secondName = "--out",
+                                  .description = "Sets the output file",
+                                  .needsValue = true});
+
+  cppli::ParseResult result = cppli::parseArguments(argc, argv, options);
+
+  // Querying the results
+
+  // This API will definitely change.
+  if (result.options.contains(ArgumentType::Help)) {
+    std::println("Help message!");
+    return EXIT_SUCCESS;
+  }
+
+  if (result.options.contains(ArgumentType::Output)) {
+    if (auto value = result.options.at(ArgumentType::Output).value) {
+      std::println("Output value is: {}", *value);
+    } else {
+      std::println("No value provided for --output");
+      return EXIT_FAILURE;
+    }
+  }
+
+  return EXIT_SUCCESS;
+}
--- example/help_example.cpp
+++ example/help_example.cpp
@@ -3,6 +3,32 @@
 auto main() -> int {
   cppli::OptionContainer<std::string> opts;
 
+  // Generated help message for this file can be found at the bottom of the
+  // file.
+
+  // As of the current state the help message consists of 3 secions: USAGE,
+  // EXAMPLE, OPTIONS
+  // USAGE contains a simple definition on how to use the program
+  // USAGE:
+  // <programName> [OPTIONS] [POS_ARGS]
+  //
+  //
+  // Example section is user defined as an argument to the createHelp function.
+  // EXAMPLE:
+  // <Whatever_user_inputs_as_arg>
+  //
+  // The most important part - options.
+  // OPTIONS:
+  //
+  // each option's entry looks like this
+  //                COLUMN 1                             |   COLUMN 2
+  //  <FIRST_NAME>, <SECOND_NAME> ["<ARG>" if needsValue] <DESCRIPTION>
+  //
+  // Column 1 and Column 2 have fixed widths, and each column's text will be
+  // placed only in their corresponding column, and will "wrap" to the next line
+  // of the column if it doesn't fit in one line.
+
+  // For each option description field is defined.
   opts.addOption("opt1", cppli::Option{.firstName = "-h",
                                        .secondName = "--help",
                                        .description = "Example description",
@@ -12,13 +38,15 @@ auto main() -> int {
                   .firstName = "-o",
                   .secondName = "--opt",
                   .description =
-                      "This should be a very very long description spanning "
-                      "multiple lines. I Could talk about how much I "
-                      "appreciate this option. It introduces so much freshness "
-                      "into this program and allows to do amazing things. "
-                      "LONGWORDISSPLITAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
-                      "AAAAAAAAAAAAAAAAAAAAAAAAAAA"
-                      "aaaaaaaaaaaaaaaaaaaaaaaaa",
+                      // Words are not split (as long as their length is shorter
+                      // or equal to the length of the description column)
+                  "This should be a very very long description spanning "
+                  "multiple lines. I Could talk about how much I "
+                  "appreciate this option. It introduces so much freshness "
+                  "into this program and allows to do amazing things. "
+                  "LONGWORDISSPLITAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+                  "AAAAAAAAAAAAAAAAAAAAAAAAAAA"
+                  "aaaaaaaaaaaaaaaaaaaaaaaaa",
                   .needsValue = false});
   opts.addOption(
       "opt3",
@@ -35,3 +63,40 @@ auto main() -> int {
 
   std::println("Generated help message:\n\n{}", help);
 }
+
+// clang-format off
+//
+// Output of this file when executed.
+
+//Generated help message:
+//
+//USAGE:
+//cool-program [OPTIONS] [POS_ARGS]
+//
+//
+//EXAMPLE:
+//cool-program --new-lines=10 -o
+//cool-program --new-lines 10 -o hello-im-positional
+//
+//OPTIONS:
+//   -n, --new-lines <ARG>    I
+//                            wanna
+//                            Do
+//                            Multiple
+//                            Lines
+//                            Because
+//                            I
+//                            Can
+//
+//   -o, --opt                This should be a very very long description spanning multiple lines. I
+//                            Could talk about how much I appreciate this option. It introduces so
+//                            much freshness into this program and allows to do amazing things.
+//                            LONGWORDISSPLITAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+//                            AAAAAAAAAAaaaaaaaaaaaaaaaaaaaaaaaaa
+//
+//   -h, --help               Example description
+//
+//
+//
+
+// clang-format on
